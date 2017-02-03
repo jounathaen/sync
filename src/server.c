3438a52f63a7744f635a403e21cf04fe01bbb91b
Jonathan Klimt <jounathaen@mail.de>
2017-02-03 17:27:56 +0100

    Wrote Server Programm

--- src/server.c
+++ src/server.c
@@ -1,5 +1,5 @@
 #include "server.h"
-
+#include "fileList.h"
 
 void printUsage(){
   // TODO Adapt
@@ -7,17 +7,72 @@ void printUsage(){
 }
 
 int main (int argc, char** argv){
-  /* int mysocket = createSocketSending(); */
-  int mysocket = createSocketListen();
+  // TODO command line Parameters PORTNUM!!!
+  int mysocket = createSocketListen("1234");
 
-  listen(mysocket, 5);
+  fileList remoteFiles, remoteFilesToDelete, remoteFilesToTransfer;
+	if (listen(mysocket, 5) == -1) {
+		printf("Error listen Socket\n");
+		exit(1);
+	}
+
   socklen_t slen = sizeof(struct sockaddr);
   struct sockaddr_in clientaddr;
-  int currentsocket = accept(mysocket, (struct sockaddr*) &clientaddr, &slen);
+  while(1){
+    int currentsocket = accept(mysocket, (struct sockaddr*) &clientaddr, &slen);
+    if (recieveSync(currentsocket) == -1){
+      shutdown(currentsocket, 2);
+      break;
+    }
 
-  /* recieveBuf(currentsocket); */
+    /* TODO recieve directory */
+    char * directory = "test2";
+    printf ("Directory to sync: %s", directory);
 
-  recieveList(currentsocket);
+    fileListInit(&remoteFiles);
+    fileListInit(&remoteFilesToTransfer);
+    fileListInit(&remoteFilesToDelete);
 
+
+    createFileList(directory, &remoteFiles);
+    removeDirname(&remoteFiles, directory);
+    printf ("\nOwn File List of Dir: %s\n", directory);
+    printf ("===============================\n");
+    printFileList(&remoteFiles);
+
+    printf("\nsending File List...\n");
+    sendList(currentsocket, &remoteFiles);
+    printf("Done!\n");
+
+    printf("recieving File Lists...\n");
+    recieveList(currentsocket, &remoteFilesToTransfer);
+    recieveList(currentsocket, &remoteFilesToDelete);
+    printf("Done!\n");
+    printf ("\nRecieved File List to Transfer:\n");
+    printf ("===============================\n");
+    printFileList(&remoteFilesToTransfer);
+    printf ("\nRecieved File List to Delete:\n");
+    printf ("===============================\n");
+    printFileList(&remoteFilesToDelete);
+
+    printf("\nRecieving...\n");
+    recieveListFiles(currentsocket, directory);
+
+
+    printf("Deleting...\n");
+    removeFileList(&remoteFilesToDelete, directory);
+
+    printf("Sending...\n");
+    sendListFiles(currentsocket, &remoteFilesToTransfer, directory);
+
+    shutdown(currentsocket, 2);
+    free(remoteFiles.entry);
+    free(remoteFilesToTransfer.entry);
+    free(remoteFilesToDelete.entry);
+    printf("Sucess\n");
+  }
+  shutdown(mysocket, 2);
+
+  /* never reached */
   return 0;
 }
